15147e42168f88f3f231318a4a0177999cb13acb
Martin <Martin.Arend@hxx4.de>
2026-03-05 13:20:10 +0100

    Nutze Microsoft.PowerShell.SDK statt System.Management.Automation

--- AdminAssistant.Core/Interfaces/IDhcpService.cs
+++ AdminAssistant.Core/Interfaces/IDhcpService.cs
@@ -6,6 +6,7 @@ public interface IDhcpService
 {
     Task<IEnumerable<DhcpScope>> GetScopesAsync();
     Task<IEnumerable<DhcpLease>> GetLeasesAsync(string scopeId);
+    Task<bool> RemoveLeaseAsync(string scopeId, string ipAddress, string performedBy);
     Task<IEnumerable<DhcpReservation>> GetReservationsAsync(string scopeId);
     Task<bool> AddReservationAsync(DhcpReservation reservation, string performedBy);
     Task<bool> RemoveReservationAsync(string scopeId, string ipAddress, string performedBy);
